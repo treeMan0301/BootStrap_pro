d1f6d1334062308ea0e950ee5f6575b940acd1ec
treeMan0301 <treem0301@gmail.com>
2025-02-24 08:10:45 -0500

    Fix_patient_UI

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
 document.addEventListener("DOMContentLoaded", function () {
-    console.log("Dashboard Loaded");
+  console.log("Dashboard Loaded");
+  $("#zeke-profile").hide();
+  $("#sloan-profile").hide();
+  $("#alex-profile").hide();
+});
+
+$(document).ready(function () {
+  $("#zeke-profile-click").click(function () {
+    $("#initial-content").hide();
+    $("#zeke-profile").show();
+    $("#sloan-profile").hide();
+    $("#alex-profile").hide();
+  });
+  $("#sloan-profile-click").click(function () {
+    $("#initial-content").hide();
+    $("#zeke-profile").hide();
+    $("#sloan-profile").show();
+    $("#alex-profile").hide();
+  });
+  $("#alex-profile-click").click(function () {
+    $("#initial-content").hide();
+    $("#zeke-profile").hide();
+    $("#sloan-profile").hide();
+    $("#alex-profile").show();
+  });
 });
